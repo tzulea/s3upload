c1bebb009e9bd134cce57279977187fdd6212f4a
Iulian Rotaru <i.rotaru@cloudplan.net>
2023-08-21 13:47:40 +0200

    add calback for transfer and error

--- CloudBackup.cpp
+++ CloudBackup.cpp
@@ -12,12 +12,16 @@ namespace s3upload
     auto s3Cred = Aws::Auth::AWSCredentials();
     s3Cred.SetAWSAccessKeyId(aConfig.at(awsCred));
     s3Cred.SetAWSSecretKey(aConfig.at(awsSecret));
-    mS3Client = std::make_unique<Aws::S3::S3Client>(s3Cred);
+    mS3Client = std::make_shared<Aws::S3::S3Client>(s3Cred);
 
     auto nThreads = std::thread::hardware_concurrency();
     mExecutor = std::make_unique<Aws::Utils::Threading::PooledThreadExecutor>(nThreads);
 
     Aws::Transfer::TransferManagerConfiguration config(mExecutor.get());
+    config.s3Client = mS3Client;
+    config.errorCallback = std::bind(&CloudBackup::onUploadError, this, std::placeholders::_1, std::placeholders::_2, std::placeholders::_3);
+    config.uploadProgressCallback = std::bind(&CloudBackup::onUploadProgress, this, std::placeholders::_1, std::placeholders::_2);
+
     mTransfer = Aws::Transfer::TransferManager::Create(config);
   }
   CloudBackup::~CloudBackup()
@@ -25,4 +29,12 @@ namespace s3upload
     Aws::SDKOptions options;
     Aws::ShutdownAPI(options);
   }
+  void CloudBackup::onUploadProgress(const Aws::Transfer::TransferManager* aManager, const std::shared_ptr<const Aws::Transfer::TransferHandle>& aHandle)
+  {
+  }
+
+  void CloudBackup::onUploadError(const Aws::Transfer::TransferManager* aManager, const std::shared_ptr<const Aws::Transfer::TransferHandle>& aHandle, const Aws::Client::AWSError<Aws::S3::S3Errors>& aError)
+  {
+  }
+
 }
--- Cloudbackup.h
+++ Cloudbackup.h
@@ -1,13 +1,7 @@
 #pragma once
 #include "aws/transfer/TransferManager.h"
 #include <memory>
-namespace Aws
-{
-  namespace Utils
-  {
-    //class 
-  }
-}
+
 namespace s3upload
 {
   class CloudBackup
@@ -16,9 +10,14 @@ namespace s3upload
     CloudBackup(const std::map<std::string, std::string>& aConfig);
     ~CloudBackup();
   private:
+    void onUploadProgress(const Aws::Transfer::TransferManager* aManager, const std::shared_ptr<const Aws::Transfer::TransferHandle>& aHandle);
+    void onUploadError(const Aws::Transfer::TransferManager* aManager, const std::shared_ptr<const Aws::Transfer::TransferHandle>& aHandle, const Aws::Client::AWSError<Aws::S3::S3Errors>& aError);
+
     std::unique_ptr<Aws::Utils::Threading::PooledThreadExecutor> mExecutor;
-    std::unique_ptr<Aws::S3::S3Client> mS3Client;
+    std::shared_ptr<Aws::S3::S3Client> mS3Client;
     std::shared_ptr<Aws::Transfer::TransferManager> mTransfer;
+
+
   };
 
 }
